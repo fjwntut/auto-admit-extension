eacd8878c290b57de78d03435a74fe60f820b3e8
fjwntut <t106ac1027@ntut.org.tw>
2021-05-22 19:10:12 +0800

    remove click icon action

--- source/js/background.js
+++ source/js/background.js
@@ -1,4 +1,4 @@
-var toggle="off";
+var toggle="on";
 var active = "_inactive";
 function updateExtState(isActive,isOn){
     // update icon
@@ -8,47 +8,20 @@ function updateExtState(isActive,isOn){
             "38": "./res/icons_"+isOn+isActive+"/icon-32.png",
         }
     });
+    chrome.browserAction.setBadgeText({text: ""})
+    //chrome.browserAction.setBadgeText({text: toggle})
 
-    // send state to content_script
-    if(active == ""){
-        chrome.tabs.query({
-            active: true, currentWindow: true
-        }, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {
-                action: "toggle",
-                value: toggle
-            }, function(response) {});  
-        });
-    }
 }
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.action === "updateIcon") {
         active = msg.value?"":"_inactive";
         updateExtState(active, toggle);
-        //sendResponse({result: toggle});
     }
-});
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.browserAction.setBadgeText({text: ""});
-    
-    switch (toggle) {
-        case "off":
-            toggle = "on";
-            //chrome.browserAction.setBadgeBackgroundColor({color: "#00ac47"});
-            break;
-        case "on":
-            toggle = "off";
-            //chrome.browserAction.setBadgeBackgroundColor({color: "#2684fc"});
-            break;
-        case "once":
-            toggle = "off";
-            //chrome.browserAction.setBadgeBackgroundColor({color: "#959595"});
-            break;
-        
-        default:
-            break;
+    else if (msg.action === "setToggle") {
+        toggle = msg.value;
+        updateExtState(active, toggle);
     }
-    updateExtState(active, toggle);
-    //chrome.browserAction.setBadgeText({text: toggle});
-
+    else if (msg.action === "getToggle") {
+        sendResponse({result: toggle});
+    }
 });
--- source/js/content_script.js
+++ source/js/content_script.js
@@ -2,32 +2,49 @@ var toggle;
 var button_added;
 var btn_text;
 var btn_icon;
-function autoAdmit() {
-    if(toggle){
-        for (let element of document.getElementsByTagName('span')) {
-            if (element.innerHTML === 'Admit') {
-                console.log('There is someone waiting to join this meeting, automatically admitting them...');
-                element.click();
-            }
-        }
+
+// Restore toggle state from background
+function getToggle(){
+    chrome.runtime.sendMessage({
+        action: 'getToggle'
+    },(response)=>{
+        toggle = response.result
+    })
+}
+getToggle();
+
+setInterval(Update, 500);
+function Update() {
+    if(toggle == "on"){
+        AutoClick();
     }
     if(!button_added){
-        addButton();
+        AddButton();
     }
 }
-function addButton(){
-    var newImg = document.createElement('img');
-    newImg.src = chrome.runtime.getURL("./res/button-"+toggle+".png");
-    newImg.width = 24;
-    newImg.height = 24;
-    var newDiv = document.createElement('div');
-    newDiv.innerHTML = "Auto Admit "+(toggle?"on":"off");
+
+function AutoClick() {
+    for (let element of document.getElementsByTagName('span')) {
+        if (element.innerHTML === 'Admit') {
+            console.log('There is someone waiting to join this meeting, automatically admitting them...');
+            element.click();
+        }
+    }
+}
+
+function AddButton(){
     var newBtn = document.createElement('button');
     newBtn.id = "auto-admit-button";
+
+    var newImg = document.createElement('img');
+    var newDiv = document.createElement('div');
+
     var newCSS = document.createElement("link");
     newCSS.type = "text/css";
     newCSS.rel = "stylesheet";
     newCSS.href = chrome.runtime.getURL("./res/btn.css");
+
+    // Append to html
     for(let element of document.getElementsByTagName('div')){
         if (element.getAttribute("aria-label") == "Raise hand"){
             if(!button_added){
@@ -35,33 +52,37 @@ function addButton(){
                 button_added = parent.parentElement.insertBefore(newBtn, parent);
                 btn_icon = button_added.appendChild(newImg);
                 btn_text = button_added.appendChild(newDiv);
-                console.log(newCSS.href)
                 document.head.appendChild(newCSS);
                 button_added.addEventListener("click",()=>{button_onclick()});
+                UpdateButton();
             }
         }
     }
     
 }
+
 function button_onclick(){
     toggle = (toggle=="off")?"on":"off";
+    UpdateButton();
+}
+
+function UpdateButton(){
     btn_icon.src = chrome.runtime.getURL("./res/button-"+toggle+".png");
     btn_text.innerHTML = "Auto Admit "+toggle;
+    chrome.runtime.sendMessage({
+        action: 'setToggle',
+        value: toggle
+    });
     console.log("Auto admit "+toggle);
 }
+
+// Change icon color when tab active
+sendState(true);
+window.onfocus = () => {sendState(true);};
+window.onblur = () => {sendState(false)};
 function sendState(isActive) {
     chrome.runtime.sendMessage({
         action: 'updateIcon',
         value: isActive
     });
 }
-sendState(true);
-window.onfocus = () => {sendState(true);};
-window.onblur = () => {sendState(false)};
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) =>{
-    if (msg.action == "toggle") {
-        toggle = msg.value;
-    }
-    console.log("Auto admit toggle: "+toggle);
-});
-setInterval(autoAdmit, 500);
